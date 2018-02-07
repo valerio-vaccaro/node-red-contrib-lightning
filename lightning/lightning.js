88a53562019b177137c6d9e572b36631c6b6e123
Gianluca Mazza <gianluca@bhb.network>
2018-02-07 17:07:04 +0100

    update new methods

--- lightning/lightning.js
+++ lightning/lightning.js
@@ -19,51 +19,68 @@
 
 //import the requirements
 const LightningClient = require('lightning-client');
+const lightning_dir = "/home/satoshi/.lightning";
 
 module.exports = function(RED) {
   // Node for stamp a Buffer content
-  function lightning_newaddr(n) {
+  function newaddr(n) {
     RED.nodes.createNode(this, n);
-    this.status({
-      fill: "grey",
-      shape: "dot",
-      text: "Waiting ..."
-    });
-    var msg = {};
     var node = this;
     this.on("input", function(msg) {
-      const client = new LightningClient('/home/bitcoind/.lightning');
-      
+      const client = new LightningClient(lightning_dir);
       client.newaddr()
         .then(addr => { 
-          this.status({
-            fill: "green",
-            shape: "dot",
-            text: "Done"
-          });
           msg.payload = addr; 
           node.send(msg);
         });
-
-
-      // import the file content like Buffer
-      /*const stampResultPromise = OpenTimestamps.stamp(msg.fileArray);
-      stampResultPromise.then(stampResult => {
-        // convert the stampResult in a Buffer
-        var ots = new Buffer(stampResult);
-        msg.otsArray = ots;
-        this.status({
-          fill: "green",
-          shape: "dot",
-          text: "Done"
-        });
-        node.send(msg);
-      });*/
     });
-  }
+  };
+
+  function listinvoices(n) {
+    RED.nodes.createNode(this, n);
+    var node = this;
+    this.on("input", function(msg) {
+      const client = new LightningClient(lightning_dir);
+      client.listinvoices()
+        .then(addr => {
+          msg.payload = addr;
+          node.send(msg);
+        });
+    });
+  };
+
+ function waitanyinvoice(n) {
+    RED.nodes.createNode(this, n);
+    var node = this;
+    this.on("input", function(msg) {
+      const client = new LightningClient(lightning_dir);
+      client.waitanyinvoice(msg.id)
+        .then(addr => {
+          msg.payload = addr;
+          node.send(msg);
+        });
+    });
+  };
+
+ function invoice(n) {
+    RED.nodes.createNode(this, n);
+    var node = this;
+    this.on("input", function(msg) {
+      const client = new LightningClient(lightning_dir);
+      console.log(msg);
+      client.invoice(msg.msatoshi,msg.label)
+        .then(addr => {
+	  console.log(addr);
+          msg.payload = addr;
+          node.send(msg);
+        });
+    });
+  };
 
   // Register the node by name. This must be called before overriding any of the
   // Node functions.
-  RED.nodes.registerType("lightning_newaddr", lightning_newaddr);
-
+  RED.nodes.registerType("newaddr", newaddr);
+  RED.nodes.registerType("listinvoices", listinvoices);
+  RED.nodes.registerType("waitanyinvoice", waitanyinvoice);
+  RED.nodes.registerType("invoice", invoice);
 }
